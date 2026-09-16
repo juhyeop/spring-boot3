603e5c8df56008c2104f6337e9776ba5ce5ea2b0
우잉 <juhyeopsee@gmail.com>
2026-09-16 11:47:02 +0900

    mysql

--- springboot-app/src/main/java/com/example/demo/UserController2.java
+++ springboot-app/src/main/java/com/example/demo/UserController2.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import java.util.List;
 
 @RestController
 public class UserController2 {
